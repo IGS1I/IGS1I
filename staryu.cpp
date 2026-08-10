56287b0b05d502e75df3efdeaca5aabe4e20d409
William K. Coleman <168572788+IGS1I@users.noreply.github.com>
2026-08-10 18:17:09 -0400

    Clamp credit values in getCoordinates function to ensure valid range for star shape

--- staryu.cpp
+++ staryu.cpp
@@ -43,7 +43,6 @@ struct stat {
  * @return The x-value and y-value of coordinate
  */
 Point getCoordinates(double num, double angle, double center, double radius, double maxValue) {
-    num = clamp(num, 0.0, maxValue); // clamp value (0-maxValue)
     Point coord;
     coord.x = center + (radius * (num/maxValue)) * cos(angle);
     coord.y = center + (radius * (num/maxValue)) * sin(angle);
@@ -149,7 +148,8 @@ int main () {
     svg << "<polygon class=\"star-shape\" points=\"";
     for (int i=0; i < n; ++i) {
         double angle = i * (2 * PI / n) - PI / 2;
-        Point p = getCoordinates(stats[i].credits, angle, center, radius, maxVal);
+        num = clamp(stats[i].credits, 0.0, maxValue); // clamp credit (0-maxValue)
+        Point p = getCoordinates(num, angle, center, radius, maxVal);
         svg << p.x << "," << p.y << " ";
     }
     svg << "\" fill=\"rgba(101, 112, 239, 0.93)\" stroke=\"#6e78ccc2\" stroke-width=\"2\" />\n";
